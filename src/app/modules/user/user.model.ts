e897c9735635fdc284567401adb41a56ff5bc7b1
chayan <admin@ditskill.com>
2023-12-19 16:33:43 +0600

    final commit

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from 'mongoose';
 import { User} from './user.interface';
+import bcrypt from 'bcrypt'
+import config from '../../config';
 
 const userSchema= new Schema<User>({
   userId: { type: Number, required: true },
@@ -13,5 +15,33 @@ const userSchema= new Schema<User>({
   address: { street: String, city: String, country: String },
   orders: [{ productName: String, price: Number, quantity: Number }],
 });
+//pre save middleware
+userSchema.pre('save',async function(next){
+//hash password
+
+//const user = this;
+const user = this as User;
+
+  user.password = await bcrypt.hash(user.password, Number(config.bcrypt_salt_rounds));
+
+
+
+  //console.log(this);
+  next();
+console.log(this);
+
+})
+
+//post save middleware
+userSchema.post('save',async function(doc,next){
+//hash password
+doc.password='';
+
+next();
+})
+
+
+
+
 
 export const userModel =model<User>('user',userSchema);
